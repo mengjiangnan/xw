a4d9ca2a0002f096be573ae266455f14122a7cf4
yangweiying <2093315526@qq.com>
2017-03-20 14:39:08 +0800

    The  image of icon

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
   include_once dirname (__FILE__).'/Conf/conn.php'; //引入数据库连接文件
-  include_once 'xw_index_title_class.php'; //引入首页标题类文件
+  include_once './xw_index_title_class.php'; //引入首页标题类文件
   $my_class = new index_title();
   $my_class->setTitle('xw');
   $xw_index_title = $my_class->getTitle();
-
 ?>
 <!Document>
 <html>
-   <head>
+ <head>
        <title>
            <?php
            echo $xw_index_title;
@@ -16,5 +15,11 @@
        </title>
        <!--标题图标-->
        <link rel="shortcut icon" href="./Image/icon/xw_title_icon.ico" type="image/x-icon">
+       <!--顶部导航CSS-->
+       <link rel="stylesheet" type="text/css" href="./Css/xw_index.css">
    </head>
+ <body>
+   <div class="top_nav" id="nav_outside">
+   </div>
+ </body>
 </html>
