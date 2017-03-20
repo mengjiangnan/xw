2317d12832fc6fc29ba6325fe629d79934cf2076
yangweiying <2093315526@qq.com>
2017-03-20 11:01:46 +0800

    The  image of icon

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-  include_once 'conn.php'; //引入数据库连接文件
-  include_once 'xw_index_title_class.php';
+  include_once dirname (__FILE__).'/Conf/conn.php'; //引入数据库连接文件
+  include_once 'xw_index_title_class.php'; //引入首页标题类文件
   $my_class = new index_title();
-  $my_class->setTitle('wx');
+  $my_class->setTitle('xw');
+  $xw_index_title = $my_class->getTitle();
+
 ?>
 <!Document>
 <html>
    <head>
        <title>
            <?php
-           echo $my_class->getTitle();
+           echo $xw_index_title;
            ?>
        </title>
        <!--标题图标-->
-       <link rel="shortcut icon" href="xw_icon.ico" type="image/x-icon">
+       <link rel="shortcut icon" href="./Image/icon/xw_title_icon.ico" type="image/x-icon">
    </head>
 </html>
--- xw_index_title_class.php
+++ xw_index_title_class.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * wx首页标题封装类
+ * $title变量存储标题名称
+ * setTitle函数设置标题名称
+ * getTitle函数返回标题名称
+ */
   class index_title {
       private $title;
 
