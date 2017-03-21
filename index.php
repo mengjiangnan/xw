deea223aab5af2860605e8eb929d0f1d852d939e
yangweiying <2093315526@qq.com>
2017-03-21 19:40:24 +0800

    The  image of icon

--- index.php
+++ index.php
@@ -24,41 +24,44 @@
            <ul class="top_nav_ul" id="nav_ul">
                <li class="top_logo" id="logo">
                    <a class="top_li_first" id="li_first_a" href="#">
-                       <img src="./Image/logo/xw_icon_test1.png">
+                       <!--<img src="./Image/logo/xw_icon_test1.png">-->
                    </a>
                </li>
                <li class="top_li_second" id="li_second">
-                   <a class="top_li_second" id="li_second_a" href="#">
+                   <a class="top_li_second_a" id="li_second_a" href="#">
                        我的最爱
                    </a>
                </li>
                <li class="top_li_three" id="li_three">
-                   <a class="top_li_three" id="li_three_a" href="#">
+                   <a class="top_li_three_a" id="li_three_a" href="#">
                        我关注的
                    </a>
                </li>
                <li class="top_li_four" id="li_four">
-                   <a class="top_li_four" id="li_four_a" href="#">
+                   <a class="top_li_four_a" id="li_four_a" href="#">
                        收藏夹
                    </a>
                </li>
                <li class="top_li_five" id="li_five">
-                   <a class="top_li_five" id="li_five_a" href="#">
+                   <a class="top_li_five_a" id="li_five_a" href="#">
                        手机版
                    </a>
                </li>
                <li class="top_search" id="li_search">
-                   <a class="top_search" id="li_search_a" href="#">
+                   <a class="top_search_a" id="li_search_a" href="#">
                    </a>
                </li>
                <li class="top_list" id="li_list">
-                   <a class="top_list" id="li_list_a" href="#">
+                   <a class="top_list_a" id="li_list_a" href="#">
                    </a>
                </li>
            </ul>
            <!--搜索-->
            <form action="">
                <div class="search_main">
+                   <button class="search_btn" type="submit"></button>
+                   <input class="search_text" type="text" placeholder="搜索">
+                   <span class="close_btn"></span>
                </div>
            </form>
            <!--会员登录-->
