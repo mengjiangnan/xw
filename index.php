2bdd03f303662ee8960b252ee3c0c9c9a2c0c8bb
yangweiying <2093315526@qq.com>
2017-03-24 16:23:33 +0800

    The  image of icon

--- index.php
+++ index.php
@@ -17,42 +17,44 @@
        <link rel="shortcut icon" href="./Image/icon/xw_title_icon.ico" type="image/x-icon">
        <!--顶部导航CSS-->
        <link rel="stylesheet" type="text/css" href="./Css/xw_index.css">
+       <!--jquery-3.2.0-->
+       <script type="text/javascript" src="./js/jquery-3.2.0.min.js"></script>
    </head>
  <body>
    <div class="top_nav" id="nav_outside">
        <div class="top_nav_inner" id="nav_inner">
            <ul class="top_nav_ul" id="nav_ul">
                <li class="top_logo" id="logo">
-                   <a class="top_li_first" id="li_first_a" href="#">
-                       <img src="./Image/logo/xw_icon_test1.png">
+                   <a class="top_li_first" id="li_first_a">
+                       <!--<img src="./Image/logo/xw_icon_test1.png">-->
                    </a>
                </li>
                <li class="top_li_second" id="li_second">
-                   <a class="top_li_second_a" id="li_second_a" href="#">
+                   <a class="top_li_second_a" id="li_second_a">
                        我的最爱
                    </a>
                </li>
                <li class="top_li_three" id="li_three">
-                   <a class="top_li_three_a" id="li_three_a" href="#">
+                   <a class="top_li_three_a" id="li_three_a">
                        我关注的
                    </a>
                </li>
                <li class="top_li_four" id="li_four">
-                   <a class="top_li_four_a" id="li_four_a" href="#">
+                   <a class="top_li_four_a" id="li_four_a">
                        收藏夹
                    </a>
                </li>
                <li class="top_li_five" id="li_five">
-                   <a class="top_li_five_a" id="li_five_a" href="#">
+                   <a class="top_li_five_a" id="li_five_a">
                        手机版
                    </a>
                </li>
                <li class="top_search" id="li_search">
-                   <a class="top_search_a" id="li_search_a" href="#">
+                   <a class="top_search_a">
                    </a>
                </li>
                <li class="top_list" id="li_list">
-                   <a class="top_list_a" id="li_list_a" href="#">
+                   <a>
                    </a>
                </li>
            </ul>
@@ -69,16 +71,33 @@
                <p>会员中心</p>
                <ul>
                    <li>
-                       <img alt="" src="./Image/other">
-                       <a href="#">登录</a>
+                       <img alt="" src="./Image/other_img">
+                       <a>登录</a>
                    </li>
                    <li>
-                       <img alt="" src="./Image/other">
-                       <a href="#">新会员注册</a>
+                       <img alt="" src="./Image/other_img">
+                       <a>新会员注册</a>
                    </li>
                </ul>
            </div>
        </div>
    </div>
+ <script>
+     /*搜索*/
+     $(function () {
+         $(".top_search_a").click(function () {
+             $(".top_nav_ul").fadeOut(500);
+             $(".search_main").fadeIn(1000);
+         });
+         $(".search_main .close_btn").click(function () {
+             $(".search_main").fadeOut(500);
+             $(".top_nav_ul").fadeIn(1000);
+         })
+         /*登录*/
+         $(".top_list a").click(function () {
+             $(".member").slideToggle(500);
+         });
+     });
+ </script>
  </body>
 </html>
