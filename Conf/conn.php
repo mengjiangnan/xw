0398b3e25dcf3d0185646c32384c211739a2be76
mengjiangnan <2093315526@qq.com>
2017-03-28 14:38:24 +0800

    the change of conn.php test success!

--- Conf/conn.php
+++ Conf/conn.php
@@ -5,13 +5,17 @@
  * dbname:数据库名称
  * port:端口号
  */
+
+$dbms = 'mysql'; //数据库类型
+$host = 'localhost'; //数据库主机名称
+$dbname = 'world';  //使用的数据库
+$user = 'root'; //数据库连接用户名
+$password = 'a019243a'; //数据库连接密码
+$dbport = '3306';
+$dsn = "$dbms:host=$host;dbname=$dbname;port=$dbport";
+
    try{
-       $pdo = new PDO(
-           'mysql:host=localhost;
-           db_name=world;
-           port=3306',
-           "root", //数据库登陆名称
-           "a019243a"); //数据库登陆密码
+       $dbh = new PDO($dsn, $user, $password); //初始化一个pdo对象
    }
    catch(PDOException $e){
        echo 'Connection failed: '.$e->getMessage(); //打印数据库连接异常信息
