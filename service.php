8896e3eab16c5272a9930e31afeda16483659bd6
yangweiying <2093315526@qq.com>
2017-04-10 18:06:28 +0800

    continue..

--- service.php
+++ service.php
@@ -6,7 +6,6 @@ include_once dirname (__FILE__).'/Conf/conn.php'; //引入数据库连接文件
 
 if (isset($_POST['user_name'],$_POST['user_pwd'])){
       extract($_POST);
-      $action = "";
       if ($action == 'submit_id'){
           $sql = "SELECT COUNT(id) FROM xw_user_auth WHERE user_name = '$user_name' and user_pwd = md5('$user_pwd')";
           $stmt = $dbh->prepare($sql);
