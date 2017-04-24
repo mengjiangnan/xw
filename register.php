85e5a31831b6e505d838a0e0d33b82e8e5cdf850
yangweiying <2093315526@qq.com>
2017-04-24 11:12:08 +0800

    continue..

--- register.php
+++ register.php
@@ -5,8 +5,6 @@
  * Date: 2017/4/24
  * Time: 9:18
  */
-//include ("index.php");
-//echo'register';
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -14,10 +12,16 @@
        <title>
            register
        </title>
+       <!--标题图标-->
+       <link rel="shortcut icon" href="./Image/icon/xw_title_icon.ico" type="image/x-icon">
+       <!--顶部导航CSS-->
+       <link rel="stylesheet" type="text/css" href="./Css/xw_index.css">
+       <!--jquery-3.2.0-->
+       <script type="text/javascript" src="./js/jquery-3.2.0.min.js"></script>
    </head>
    <body>
    <?php
-   include ("index.php");
+   include_once ("header.php");
    ?>
    </body>
 </html>
