e33ab7ec2913f98a3df7ba164e209956e634b8a3
yangweiying <2093315526@qq.com>
2017-04-28 15:40:09 +0800

    continue..

--- register.php
+++ register.php
@@ -51,16 +51,18 @@
        </div>
    </div>
    <script>
-   /*input框内清除按钮更改
+   /*input框内清除按钮更改*/
    var text = $(".form_user_name_input");
    var inner_input = $(".form_user_name_clear_btn_span");
-   text.focus(function () {
-   inner_input.show();
+   text.bind("input propertychange",function () {
+       if(text.val().length!==0){
+           inner_input.show();
+       }
    });
    inner_input.click(function () {
-   text.val('');
-   inner_input.hide();
-   });*/
+       text.val('');
+       inner_input.hide();
+   });
    </script>
    </body>
 </html>
