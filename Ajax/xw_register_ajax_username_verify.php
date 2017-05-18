ef082a23d69cdb36a0ae8f983782199eca603df7
yangweiying <2093315526@qq.com>
2017-05-18 16:49:28 +0800

    continue..

--- Ajax/xw_register_ajax_username_verify.php
+++ Ajax/xw_register_ajax_username_verify.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * 验证注册时，用户名是否已被占用.
- */
+session_start();
 include_once ("../Conf/conn.php"); //引入数据库连接文件
 if (!empty($_POST['form_user_name_inputname'])){
     extract($_POST);
@@ -11,7 +9,7 @@ if (!empty($_POST['form_user_name_inputname'])){
     //取得行数
     $result = $stmt ->fetchColumn();
     if ($result==1){
-        session_start();
+        //session_start();
         $_SESSION['user_name']=$form_user_name_inputname;
         $success_arr = array ('state'=>'success','user_name'=>$form_user_name_inputname);
         die(json_encode($success_arr));
--- register.php
+++ register.php
@@ -56,8 +56,9 @@ session_start(); //开启session
                    <p class="pass_suggest_item">
                        <label name="suggestName" class="suggest_test">
                            <input name="suggestName" id="pass_suggest_item_radio_01" class="pass_suggest_item_radio" type="radio" value="123">
-                           <script> document.write($('#pass_suggest_item_radio_01').val()); </script>
-                           <!--<?php echo @$_SESSION['user_name']; ?>-->
+                           <span class="test_span"></span>
+                           <!--<script> document.write($('#pass_suggest_item_radio_01').val()); </script>-->
+                           <!--<?php  echo @$_SESSION['user_name']; ?>-->
                        </label>
                    </p>
                    <p class="pass_suggest_item">
@@ -111,7 +112,11 @@ session_start(); //开启session
                $('#form_user_name_input_id').serialize()+'&active='+$(event.target).attr('id'),
                function (data) {
                    var jsondata = $.parseJSON(data);
-                   $('#pass_suggest_item_radio_01').val(jsondata.user_name);
+                   //$('#pass_suggest_item_radio_01').val(jsondata.user_name);
+                   var b = JSON.stringify(jsondata.user_name);
+                   var c = b.replace(/\"/g, "");
+                   var a = $(".test_span").text(c);
+                   alert(typeof c);
                    if(jsondata.state =='success'){
                        $(".form_user_name_repeat_notice_span").css("display","block");
                        $('.pass_suggest_name_div').css("display","block");
